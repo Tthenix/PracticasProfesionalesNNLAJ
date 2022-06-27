c716b900d65885df9288ef885a230e56c4f9024c
Nahuel <nahuelfacundox@gmail.com>
2022-06-27 08:52:56 -0300

    imagenes interactivas

--- nnlaj_fe/src/components/plantas/camote.js
+++ nnlaj_fe/src/components/plantas/camote.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "./plantas.css"
 
 function Camote() {
   return (
-  <div align="center">
-    camotes
+  <div class="txtCamote">
+    <p>Lorem ipsum dolor sit amet. Et quidem similique est sapiente corporis sit rerum corporis. Et recusandae doloribus et sapiente ullam nam delectus obcaecati et quos voluptate qui obcaecati soluta vel dicta blanditiis. </p>
   </div>
   )
 }
