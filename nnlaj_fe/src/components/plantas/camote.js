cf2eaba7276ce5b5f96136758c8082bdae9b4db7
TtheNix <nahuelfacundox@gmail.com>
2022-08-08 12:00:25 -0300

    home final

--- nnlaj_fe/src/components/plantas/camote.js
+++ nnlaj_fe/src/components/plantas/camote.js
@@ -2,14 +2,28 @@ import React from 'react';
 import "./plantas.css";
 
 function Camote() {
-  return (
-  <div class="txtCamote">
-    <h2 >Camote</h2>
-    <p>Lorem ipsum dolor sit amet. Et quidem similique est sapiente corporis sit rerum corporis. Et recusandae doloribus et sapiente ullam nam delectus obcaecati et quos voluptate qui obcaecati soluta vel dicta blanditiis. </p>
-    <h2>Lorem ipsum</h2>
-    <p>Lorem ipsum dolor sit amet. Et quidem similique est sapiente corporis sit rerum corporis. Et recusandae doloribus et sapiente ullam nam delectus obcaecati et quos voluptate qui obcaecati soluta vel dicta blanditiis.</p>
-  </div>
-  )
+  
+
+  // <div class="titulo"> 
+  //   Descripcion
+  // </div>
+
+  // <div class='titulo2'> 
+  // Cultivo
+  // </div>
+
+  // <div class='titulo3'> 
+  // Cuidados
+  // </div>
+
+  // <div class='titulo'> 
+  // Recomendaciones
+  // </div>
+
+  // <div class='titulo'> 
+  // Reproduccion
+  // </div>
+  
 }
 
 export default Camote
