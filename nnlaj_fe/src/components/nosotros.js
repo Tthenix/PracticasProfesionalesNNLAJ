b00eaa916504413599216dd2b4d126d3c68e3947
nbeltran1 <n.beltran@alumno.etec.um.edu.ar>
2022-08-08 09:56:08 -0300

    Biografia

--- nnlaj_fe/src/components/nosotros.js
+++ nnlaj_fe/src/components/nosotros.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import "./nosotros.css"
 
 function Nosotros() {
   return (
-  <div align="center">
-    sobre nosotros
-  </div>
+    <div align="center">
+      <div>
+        <p class="nosotros1">
+          somos un equipo de 5 personas formado por:
+        </p>
+      </div>
+      <div class="nosotros2">
+        <p>
+          Nahuel Quiroga(Lider)
+        </p>
+        <p>
+          Nicolas Beltran(front end)
+        </p>
+        <p>
+          Ariel Cohen(Front end)
+        </p>
+        <p>
+          Lucas Moran(Back end)
+        </p>
+        <p>
+          Juan Yañez(busqueda de informacion)
+        </p>
+      </div>
+      <a href="https://github.com/Tthenix/PracticasProfesionalesNNLAJ">
+        https://github.com/Tthenix/PracticasProfesionalesNNLAJ
+        </a>
+    </div>
   )
 }
 
