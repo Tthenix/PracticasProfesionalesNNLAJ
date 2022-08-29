7736ba12e1cdb7e5770f1268ab31806f558b473c
lucas-hm <80957998+lucas-hm@users.noreply.github.com>
2022-08-29 08:26:24 -0300

    dev-luca

--- nnlaj_be/index.js
+++ nnlaj_be/index.js
@@ -1,6 +1,29 @@
 const { Client } = require("pg");
-const client = new Client();
-await client.connect();
-const res = await client.query("SELECT * from prueba");
-console.log(res.rows[0].message); // Hello world!
-await client.end();
+
+const obtenerCategorias = async () => {
+  const client = new Client();
+
+  await client.connect();
+
+  const res = await client.query("SELECT * from prueba");
+
+  const result = res.rows[0].message;
+  console.log(); // Hello world!
+
+  await client.end();
+
+  return result;
+};
+obtenerCategorias().then((result) => {
+  console.log(result);
+});
+///INFORMACIÓN DE PLANTAS
+///DESKTOP-TM847KF
+///zapallo
+///frutilla
+///romanescu
+///dragon
+///ají
+///ajo
+///lechuga
+///espinaca
---
+++ nnlaj_be/node modules 2.sql
@@ -0,0 +1,11 @@
+CREATE table invernadero(
+	id plantas int not NULL,
+	zapallo varchar (20),
+	frutilla varchar (20),
+	lechuga varchar (20),
+	espinaca varchar (20),
+	ají varchar (20),
+	ajo varchar (20),
+	romanescu varchar (20),
+	dragon varchar (20),
+)
