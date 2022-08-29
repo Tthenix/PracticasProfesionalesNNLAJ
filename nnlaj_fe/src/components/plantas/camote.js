6d387fa48c8370add5ca3738b820cf9cd974a14f
TtheNix <nahuelfacundox@gmail.com>
2022-08-29 08:45:12 -0300

    home y camote

--- nnlaj_fe/src/components/plantas/camote.js
+++ nnlaj_fe/src/components/plantas/camote.js
@@ -1,29 +1,54 @@
-import React from 'react';
+import React from 'react'
 import "./plantas.css";
+import fondo from "../../images/camoteImage.jpg";
+// git merge
+
 
 function Camote() {
-  
-
-  // <div class="titulo"> 
-  //   Descripcion
-  // </div>
-
-  // <div class='titulo2'> 
-  // Cultivo
-  // </div>
-
-  // <div class='titulo3'> 
-  // Cuidados
-  // </div>
-
-  // <div class='titulo'> 
-  // Recomendaciones
-  // </div>
-
-  // <div class='titulo'> 
-  // Reproduccion
-  // </div>
-  
+  return (
+  <div align="center">
+    <div class="planta1"> 
+      Camote
+    </div>
+    <div class="titulo">
+      Descripcion
+    </div>
+    <div class="texto"> 
+    Lorem ipsum dolor sit amet consectetur adipiscing, elit interdum elementum dui laoreet purus torquent, ullamcorper nostra at morbi himenaeos. Vestibulum proin ornare nibh cursus porttitor volutpat ad, fames aenean dictumst praesent facilisis per placerat curae, habitasse suscipit nulla vulputate nec lacus. 
+    </div>
+    <div class="titulo"> 
+    cultivo
+    </div>
+    <div class="texto"> 
+    Lorem ipsum dolor sit amet consectetur adipiscing, elit interdum elementum dui laoreet purus torquent, ullamcorper nostra at morbi himenaeos. Vestibulum proin ornare nibh cursus porttitor volutpat ad, fames aenean dictumst praesent facilisis per placerat curae, habitasse suscipit nulla vulputate nec lacus. 
+    </div> 
+    <div class="titulo">
+    cuidados
+    </div>
+    <div class="texto"> 
+    Lorem ipsum dolor sit amet consectetur adipiscing, elit interdum elementum dui laoreet purus torquent, ullamcorper nostra at morbi himenaeos. Vestibulum proin ornare nibh cursus porttitor volutpat ad, fames aenean dictumst praesent facilisis per placerat curae, habitasse suscipit nulla vulputate nec lacus. 
+    </div>
+    <div class="titulo">
+    recomendaciones
+    </div>
+    <div class="texto"> 
+    Lorem ipsum dolor sit amet consectetur adipiscing, elit interdum elementum dui laoreet purus torquent, ullamcorper nostra at morbi himenaeos. Vestibulum proin ornare nibh cursus porttitor volutpat ad, fames aenean dictumst praesent facilisis per placerat curae, habitasse suscipit nulla vulputate nec lacus. 
+    </div>
+    <div class="titulo">
+    reproduccion
+    </div>
+    <div class="texto"> 
+    Lorem ipsum dolor sit amet consectetur adipiscing, elit interdum elementum dui laoreet purus torquent, ullamcorper nostra at morbi himenaeos. Vestibulum proin ornare nibh cursus porttitor volutpat ad, fames aenean dictumst praesent facilisis per placerat curae, habitasse suscipit nulla vulputate nec lacus. 
+    </div>
+    <div class="fondo">
+      <img src={fondo} alt="Fondo" width="70%" height="40%"  />
+    </div>
+  </div>
+  )
 }
 
 export default Camote
+
+ 
+
+
