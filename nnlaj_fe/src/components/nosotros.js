47a392d2a6451e68078a8ee3d01b4c941bfca349
nicolasbeltran1 <n.beltran@alumno.etec.um.edu.ar>
2022-09-19 09:41:44 -0300

    Nosotros

--- nnlaj_fe/src/components/nosotros.js
+++ nnlaj_fe/src/components/nosotros.js
@@ -9,15 +9,15 @@ function Nosotros() {
           somos un equipo de 5 personas formado por:
         </p>
       </div>
-      <div class="nosotros2 text-start">
+      <div class="nosotros2">
         <p>
           Nahuel Quiroga(Lider)
         </p>
         <p>
-          Nicolas Beltran(front end)
+          Nicolas Beltran(front end,ayudante)
         </p>
         <p>
-          Ariel Cohen(Front end)
+          Ariel Cohen(Front end,ayudante)
         </p>
         <p>
           Lucas Moran(Back end)
@@ -30,8 +30,16 @@ function Nosotros() {
         <p>
           este proyecto empezo como un trabajo obligatorio pero con el tiempo nos empezo a gustar avanzarlo y hacerlo mucho mas atractivo visualmente
         </p>
+        <div>
+          <p>
+            El proyecto trata de una pagina que sirve para obter informacion hacerca de plantas (Cuidados, consejos y informacion)
+          </p>
+          <p>
+            Sobre todo si necesitas ayuda para tus plantas este sitio es el indicado
+          </p>
+        </div>
       </div>
-      <a href="https://github.com/Tthenix/PracticasProfesionalesNNLAJ">
+      <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><a class="link" href="https://github.com/Tthenix/PracticasProfesionalesNNLAJ">
         https://github.com/Tthenix/PracticasProfesionalesNNLAJ
       </a>
     </div>
