7210eea4b0632ffe0956f5e8882b9df1032b3e8b
lucas-hm <80957998+lucas-hm@users.noreply.github.com>
2022-11-07 08:48:28 -0300

    API

---
+++ init.js
@@ -0,0 +1,14 @@
+import { hello } from "conexion.js";
+import React from "react";
+
+export const hello = () => {
+  return <div>hello</div>;
+};
+document.getElementById("hola").innerHTML = "hola";
+alert("pagina =)");
+
+function app() {
+  return <div className="app.js">hello</div>;
+}
+
+export default app.js;
