6549d3426398c22c8d2c76b4676e852ff0adc458
lucas-hm <80957998+lucas-hm@users.noreply.github.com>
2022-11-07 11:22:27 -0300

    Add files via upload

---
+++ plantas.py
@@ -0,0 +1,10 @@
+plantas = [
+    {"nombre":"Zapallo", "humedad":"18° - 24°", "clima":"Arido", "temperatura":"32°"},
+    {"nombre":"Frutilla", "humedad":"15° - 20°", "clima":"Arido", "temperatura":"35°"},
+    {"nombre":"Romanesco", "humedad":"15° - 24°", "clima":"Arido", "temperatura":"24°"},
+    {"nombre":"Dragon", "humedad":"19° - 23°", "clima":"Arido", "temperatura":"28°"},
+    {"nombre":"Ají", "humedad":"14° - 20°", "clima":"Arido", "temperatura":"12°"},
+    {"nombre":"Ajo", "humedad":"5° - 18°", "clima":"Arido", "temperatura":"16°"},
+    {"nombre":"Lechuga", "humedad":"16° - 28°", "clima":"Arido", "temperatura":"27°"},
+    {"nombre":"Espinaca", "humedad":"23° - 27°", "clima":"Arido", "temperatura":"22°"}
+]
