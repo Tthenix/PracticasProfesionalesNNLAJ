271e4340558d07addeac91793961590d108718fb
atalice <a.talice@alumno.etec.um.edu.ar>
2022-11-07 09:42:21 -0300

    nosotros_terminado

--- nnlaj_fe/src/components/nosotros.js
+++ nnlaj_fe/src/components/nosotros.js
@@ -1,49 +1,64 @@
-import React from 'react'
-import "./nosotros.css"
+import React from "react";
+import "./Nosotros.css";
+import "./nosotros1.css";
 
-function Nosotros() {
-  return (
-    <div align="center">
-      <div>
-        <p class="nosotros1">
-          somos un equipo de 5 personas formado por:
-        </p>
-      </div>
-      <div class="nosotros2">
-        <p>
-          Nahuel Quiroga(Lider)
-        </p>
-        <p>
-          Nicolas Beltran(front end,ayudante)
-        </p>
-        <p>
-          Ariel Cohen(Front end,ayudante)
-        </p>
-        <p>
-          Lucas Moran(Back end)
-        </p>
-        <p>
-          Juan Yañez(busqueda de informacion)
-        </p>
-      </div>
-      <div>
-        <p>
-          este proyecto empezo como un trabajo obligatorio pero con el tiempo nos empezo a gustar avanzarlo y hacerlo mucho mas atractivo visualmente
-        </p>
-        <div>
-          <p>
-            El proyecto trata de una pagina que sirve para obter informacion hacerca de plantas (Cuidados, consejos y informacion)
-          </p>
-          <p>
-            Sobre todo si necesitas ayuda para tus plantas este sitio es el indicado
-          </p>
-        </div>
-      </div>
-      <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><a class="link" href="https://github.com/Tthenix/PracticasProfesionalesNNLAJ">
-        https://github.com/Tthenix/PracticasProfesionalesNNLAJ
-      </a>
+function nosotros() {
+    return (
+    <div>
+        <section class="u-align-center u-clearfix u-section-1" id="sec-dea7">
+            <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+                <div class="u-expanded-width u-list u-list-1">
+                <div class="u-repeater u-repeater-1">
+                    <div class="u-align-center u-container-style u-list-item u-repeater-item u-list-item-1">
+                    <div class="u-container-layout u-similar-container u-container-layout-1">
+                        <div alt="" class="u-image u-image-circle u-image-1" src="" data-image-width="500" data-image-height="500"></div>
+                        <h5 class="u-text u-text-body-alt-color u-text-1">Nahuel Quiroga</h5>
+                        <p class="u-text u-text-body-alt-color u-text-2">Lider, Front end y Diseño</p>
+                    </div>
+                    </div>
+                    <div class="u-align-center u-container-style u-list-item u-repeater-item u-list-item-2">
+                    <div class="u-container-layout u-similar-container u-container-layout-2">
+                        <div alt="" class="u-image u-image-circle u-image-2" src="" data-image-width="500" data-image-height="500"></div>
+                        <h5 class="u-text u-text-body-alt-color u-text-3">Ariel Cohen</h5>
+                        <p class="u-text u-text-body-alt-color u-text-4">Front end, Diseño y ayudante general</p>
+                    </div>
+                    </div>
+                    <div class="u-align-center u-container-style u-list-item u-repeater-item u-list-item-3">
+                    <div class="u-container-layout u-similar-container u-container-layout-3">
+                        <div alt="" class="u-image u-image-circle u-image-3" src="" data-image-width="500" data-image-height="500"></div>
+                        <h5 class="u-text u-text-body-alt-color u-text-5">Nicolas Beltran</h5>
+                        <p class="u-text u-text-body-alt-color u-text-6"> Front end, Diseño y ayudante general</p>
+                    </div>
+                    </div>
+                    <div class="u-align-center u-container-style u-list-item u-repeater-item u-list-item-4">
+                    <div class="u-container-layout u-similar-container u-container-layout-4">
+                        <div alt="" class="u-image u-image-circle u-image-4" src="" data-image-width="500" data-image-height="500"></div>
+                        <h5 class="u-text u-text-body-alt-color u-text-7">Lucas Moran</h5>
+                        <p class="u-text u-text-body-alt-color u-text-8">Back end</p>
+                    </div>
+                    </div>
+                    <div class="u-align-center u-container-style u-list-item u-repeater-item u-list-item-5">
+                    <div class="u-container-layout u-similar-container u-container-layout-5">
+                        <div alt="" class="u-image u-image-circle u-image-5" src="" data-image-width="500" data-image-height="500"></div>
+                        <h5 class="u-text u-text-body-alt-color u-text-9">Juan Yañez</h5>
+                        <p class="u-text u-text-body-alt-color u-text-10">Busqueda de informacion, diseño</p>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </section>
+            <section class="u-align-center u-clearfix u-section-2" id="sec-a69f">
+            <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+                <div class="u-container-style u-group u-group-1">
+                <div class="u-container-layout u-container-layout-1">
+                    <h2 class="u-text u-text-body-alt-color u-text-default u-text-1">El Huerto</h2>
+                    <p class="u-text u-text-body-alt-color u-text-2">Esta es una pagina la cual vas a poder encontrar mucha informacion, cuidados y mas hacerca de plantas, nuestro objetivo es llegar a poner muchos tipos de plantas, para que cualquiera que lo necesite pueda obtener informacion de lo que necesite.</p>
+                </div>
+                </div>
+            </div>
+            </section>
     </div>
-  )
+    );
 }
-
-export default Nosotros
+export default nosotros;
